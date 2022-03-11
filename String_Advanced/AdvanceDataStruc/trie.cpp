1397c4abe4e6bbeda1ed735de8f41d4008f0f645
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-03-11 13:09:20 -0500

    Start

---
+++ String_Advanced/AdvanceDataStruc/trie.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+
+//Trie Class
+class Trie 
+{
+    //Constructor for a Trie node
+
+
+
+
+
+    //Node class for the Trie Constructor
+    class Node {
+        public:
+            char c;
+            bool isWord;
+            Node[] children;
+        
+            
+
+    };
+    
+};
+
+//Main function for testing
+int main()
+{
+
+
+
+
+    return 0;
+};
